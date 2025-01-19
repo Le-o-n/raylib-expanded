34205baaff3128fe23f9fa15bec8a3018113b80f
Leon Bass <leonandersonbass@gmail.com>
2025-01-19 01:53:44 +0000

    added generics to hashmap and linked list, allowing custom comparison functions and free functions

--- include/data_structures/linked_list.h
+++ include/data_structures/linked_list.h
@@ -10,6 +10,7 @@
  */
 typedef struct LinkedList_Node {
     void *data;
+    int (*data_free_function)(void*);
     struct LinkedList_Node *next;
     struct LinkedList_Node *prev;
 } LinkedList_Node;
@@ -20,25 +21,33 @@ typedef struct LinkedList_Node {
 typedef struct LinkedList_List {
     LinkedList_Node *head;
     LinkedList_Node *tail;
+    int (*data_free_function)(void*);
     size_t size;
 } LinkedList_List;
 
+int LinkedList_genericFree(void* data); // used if data is just some contiguous memory
+int LinkedList_noFree(void* data);      // used for primatives
+
 // Function prototypes for LinkedList_Node
-void LinkedList_Node_init(LinkedList_Node* node, void *data);
-void LinkedList_Node_unload(LinkedList_Node *node);
-void LinkedList_Node_unloadAll(LinkedList_Node *node);
+int LinkedList_Node_init(LinkedList_Node* node, int (*data_free_function)(void*), void *data);
+int LinkedList_Node_delete(LinkedList_Node *node);
+int LinkedList_Node_deleteWithData(LinkedList_Node *node);
 
 // Function prototypes for LinkedList_List
-void LinkedList_List_init(LinkedList_List* list);
-void LinkedList_List_unload(LinkedList_List* list);
-void LinkedList_List_unloadAll(LinkedList_List* list);
+int LinkedList_List_init(LinkedList_List* list, int (*data_free_function)(void*));
+int LinkedList_List_delete(LinkedList_List* list); // frees struct
+int LinkedList_List_deleteWithData(LinkedList_List* list); // frees struct + frees nodes
 int LinkedList_List_pushHead(LinkedList_List* list, void *data);
 int LinkedList_List_pushTail(LinkedList_List* list, void *data);
 void* LinkedList_List_popTail(LinkedList_List* list);
 void* LinkedList_List_popHead(LinkedList_List* list);
-LinkedList_Node* LinkedList_List_find(LinkedList_List* list, void *data, int (*comparator)(void *, void *));
-size_t LinkedList_List_size(LinkedList_List* list);
+LinkedList_Node* LinkedList_List_findNode(LinkedList_List* list, void *data, int (*comparator)(void *, void *));
+void* LinkedList_List_find(LinkedList_List* list, void *data, int (*comparator)(void *, void *));
+void* LinkedList_List_pop(LinkedList_List* list, void *data, int (*comparator)(void *, void *));
+int LinkedList_List_deleteFrom(LinkedList_List* list, void *data, int (*comparator)(void *other, void *data));
+int LinkedList_List_deleteFromWithData(LinkedList_List* list, void *data, int (*comparator)(void *, void *));
 
+size_t LinkedList_List_size(LinkedList_List* list);
 void* LinkedList_List_peakHead(LinkedList_List* list);
 void* LinkedList_List_peakTail(LinkedList_List* list);
 void* LinkedList_List_peakIndex(LinkedList_List* list, size_t index, bool iter_forward);
--- src/data_structures/hashmap.c
+++ src/data_structures/hashmap.c
@@ -1,96 +1,216 @@
 
 #include "data_structures/hashmap.h"
 
-void HashMap_KeyValuePair_init(
+
+int HashMap_noFree(void* data){
+    return 0;
+}
+
+int HashMap_free(void* data){
+    free(data);
+    return 0;
+}
+
+int HashMap_equals(
+    void* key01, 
+    void* key02
+){
+    return (int)(key01!=key02);
+}
+
+
+int HashMap_KeyValuePair_init(
     HashMap_KeyValuePair* pair,
     void* key,
-    void* value
+    void* value,
+    int (*key_comparator_function)(void* key01, void* key02),
+    int (*key_free_function)(void* key),
+    int (*value_free_function)(void* value)
 ){
     if (!pair){
-        return;
+        return 1; //fail
     }
+
+    if (!key_free_function){
+        key_free_function = &HashMap_noFree;
+    }
+
+    if (!value_free_function){
+        value_free_function = &HashMap_noFree;
+    }
+
+    if (!key_comparator_function){
+        key_comparator_function = &HashMap_equals;
+    }
+
 
     pair->key = key;
     pair->value = value;
+    pair->key_free_function = key_free_function;
+    pair->value_free_function = value_free_function;
+    pair->key_comparator_function = key_comparator_function;
+    return 0; //success
 }
 
-void HashMap_KeyValuePair_unload(
+int HashMap_KeyValuePair_delete(
     HashMap_KeyValuePair* pair
 ){
     if (!pair){
-        return;
+        return 1; //fail
     }
     free(pair);
+    return 0;
 }
 
-void HashMap_KeyValuePair_unloadAll(
+int HashMap_KeyValuePair_deleteWithData(
     HashMap_KeyValuePair* pair
 ){
     if (!pair){
-        return;
+        return 1; //fail
     }
-    if (pair->key){
-        free(pair->key);
+
+    if (!pair->key_free_function){
+        return 2; //fail
     }
-    if (pair->value){
-        free(pair->value);
+
+    if (!pair->value_free_function){
+        return 3; //fail
     }
-    free(pair);
+
+    if (pair->key_free_function(pair->key)){
+        return 4; //fail
+    }
+    
+    if (pair->value_free_function(pair->value)){
+        return 5; //fail
+    }
+    
+    if (HashMap_KeyValuePair_delete(pair)){
+        return 6; //fail
+    }
+
+    return 0; //sucess
 }
+
+int HashMap_KeyValuePair_deleteGeneric(
+    void* pair
+){
+    return HashMap_KeyValuePair_delete(
+        (HashMap_KeyValuePair*)pair
+    );
+}
+
+int HashMap_KeyValuePair_deleteWithDataGeneric(
+    void* pair
+){
+    return HashMap_KeyValuePair_deleteWithData(
+        (HashMap_KeyValuePair*)pair
+    );
+}
+
+int HashMap_KeyValuePair_comparator(
+    HashMap_KeyValuePair* pair,
+    void* key
+){
+    return pair->key_comparator_function(pair->key, key);
+}
+
+int HashMap_KeyValuePair_comparatorGeneric(
+    void* pair,
+    void* key
+){
+    HashMap_KeyValuePair* key_value_pair = (HashMap_KeyValuePair*)pair;
+    return HashMap_KeyValuePair_comparator(key_value_pair, key);
+}
+
 // Initializes the hashmap
-void HashMap_Map_init(
+int HashMap_Map_init(
     HashMap_Map* map, 
     size_t (*hash_function)(
         void* key
     ),
-    int (*comparison_function)(
+    int (*key_comparison_function)(
         void* key01, 
         void* key02
     ),
+    int (*key_free_function)(void*),
+    int (*value_free_function)(void*),
     size_t capacity
 ){
     map->hash_function = hash_function;
-    map->comparison_function = comparison_function;
+    map->key_comparison_function = key_comparison_function;
     map->capacity = capacity;
     map->_array = (LinkedList_List*)malloc(
         sizeof(LinkedList_List) * capacity
     );
-    
+
+    if (!key_free_function){
+        key_free_function = &HashMap_noFree;
+    }
+    if (!value_free_function){
+        value_free_function = &HashMap_noFree;
+    }
+
+    map->key_free_function = key_free_function;
+    map->value_free_function = value_free_function;
+
     // Initialize each linked list in the array
     for (size_t i = 0; i < capacity; i++) {
         LinkedList_List_init(
-            &map->_array[i]
+            &map->_array[i],
+            &HashMap_KeyValuePair_deleteGeneric
         );
     }
+    return 0;
 }
 
 // Inserts a key-value pair into the hashmap
-void HashMap_Map_insert(
+int HashMap_Map_insert(
     HashMap_Map* map, 
     void* key, 
     void* value
-){
-    size_t hash_val = map->hash_function(key);
-    hash_val = hash_val % map->capacity;
+){  
+    if (!map){
+        return 1; //fail
+    }
 
-    LinkedList_List *hit_linked_list = &map->_array[hash_val];
+    if (!map->hash_function){
+        return 2; //fail
+    }
+
+    size_t hash_val = map->hash_function(key);
+    size_t linked_list_index = hash_val % map->capacity;
+
+    LinkedList_List *hit_linked_list = &map->_array[linked_list_index];
 
     HashMap_KeyValuePair* key_value_pair;
     key_value_pair = (HashMap_KeyValuePair*)malloc(
         sizeof(HashMap_KeyValuePair)
     );
 
-    HashMap_KeyValuePair_init(
+    if (!key_value_pair){
+        return 3; //fail
+    }
+
+    if (HashMap_KeyValuePair_init(
         key_value_pair,
         key,
-        value
-    );
+        value,
+        map->key_comparison_function,
+        map->key_free_function,
+        map->value_free_function
+    )){
+        return 4; //fail
+    }
 
-    LinkedList_List_pushHead(
+    if (LinkedList_List_pushHead(
         hit_linked_list,
         (void*)key_value_pair
-    );
+    )){
+        return 5; //fail
+    }
     
+    return 0;
 }
 
 // Retrieves a value by key from the hashmap
@@ -98,6 +218,14 @@ void* HashMap_Map_get(
     HashMap_Map* map, 
     void* key
 ){
+
+    if (!map){
+        return NULL; //fail
+    }
+
+    if (!map->hash_function){
+        return NULL; //fail
+    }
 
     size_t key_hash = map->hash_function(key);
     key_hash = key_hash % map->capacity;
@@ -111,7 +239,7 @@ void* HashMap_Map_get(
         HashMap_KeyValuePair* key_value_pair;
         key_value_pair = (HashMap_KeyValuePair*)cur_node->data;
 
-        size_t key_comparison = map->comparison_function(
+        int key_comparison = map->key_comparison_function(
             key, 
             key_value_pair->key
         );
@@ -120,7 +248,6 @@ void* HashMap_Map_get(
             return key_value_pair->value;
         }
 
-
         cur_node = cur_node->next;
     }
 
@@ -128,29 +255,108 @@ void* HashMap_Map_get(
 }
 
 // Deletes a key-value pair from the hashmap
-void HashMap_Map_delete(
+int HashMap_Map_deleteFrom(
     HashMap_Map* map, 
     void* key
 ){
-    
+    if (!map){
+        return 1; //fail
+    }
+
+    if (!map->hash_function){
+        return 2; //fail
+    }
+
+    size_t hash_val = map->hash_function(key);
+    size_t linked_list_index = hash_val % map->capacity;
+
+    LinkedList_List *hit_linked_list = &map->_array[linked_list_index];
+
+    if (LinkedList_List_deleteFrom(
+        hit_linked_list, 
+        key, 
+        &HashMap_KeyValuePair_comparatorGeneric
+    )){
+        return 2; //fail
+    }
+
+    return 0; //success
+
 }
 
-// Frees the hashmap and its resources
-void HashMap_Map_unload(HashMap_Map* map) {
-    for (size_t i = 0; i < map->capacity; i++) {
-        LinkedList_Node* current_node = map->_array[i].head;
-        LinkedList_Node* next;
-
-        HashMap_KeyValuePair* key_value_pair;
-        // Free each key-value pair in the linked list
-        while (current_node != NULL) {
-            next = current_node->next; // Store the next node
-            key_value_pair = current_node->data;
-            HashMap_KeyValuePair_unloadAll(key_value_pair);  // Free the key-value pair
-            LinkedList_Node_unloadAll(current_node);        // Free the linked list node
-            current_node = next;      // Move to the next node
-        }
+int HashMap_Map_deleteFromWithData(
+    HashMap_Map* map, 
+    void* key
+){
+    if (!map){
+        return 1; //fail
     }
+
+    if (!map->hash_function){
+        return 2; //fail
+    }
+
+    size_t hash_val = map->hash_function(key);
+    size_t linked_list_index = hash_val % map->capacity;
+
+    LinkedList_List *hit_linked_list = &map->_array[linked_list_index];
+
+    if (LinkedList_List_deleteFromWithData(
+        hit_linked_list, 
+        key, 
+        &HashMap_KeyValuePair_comparatorGeneric
+    )){
+        return 2; //fail
+    }
+
+    return 0; //success
+
+}
+
+
+// Frees the hashmap and its resources
+int HashMap_Map_delete(HashMap_Map* map) {
+    
+    if (!map){
+        return 1; //fail
+    }
+    
+    for (size_t i = 0; i < map->capacity; i++) {
+        LinkedList_List* current_list = &map->_array[i];
+        
+        if (LinkedList_List_delete(current_list)){
+            return 2; //fail
+        }
+
+    }
+
     free(map->_array); // Free the array of linked lists
     map->_array = NULL; // Avoid dangling pointer
+
+    free(map);
+
+    return 0;
+}
+
+int HashMap_Map_deleteWithData(HashMap_Map* map) {
+    
+    if (!map){
+        return 1; //fail
+    }
+    
+    for (size_t i = 0; i < map->capacity; i++) {
+        LinkedList_List* current_list = &map->_array[i];
+        
+        if (LinkedList_List_deleteWithData(current_list)){
+            return 2; //fail
+        }
+
+    }
+
+    free(map->_array); // Free the array of linked lists
+    map->_array = NULL; // Avoid dangling pointer
+
+    free(map);
+
+    return 0;
 }
